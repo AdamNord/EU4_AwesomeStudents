0f608b51339de88dabb5bc13533c13aa2db81b76
AdamNord <adamnordin01@gmail.com>
2021-01-31 14:06:58 +0100

    project is already in remote repo, commiting more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,10 @@
 public class Hello {
     //more stuff
     //Mac user and windows users are happy
+
+    //I have already published on GitHub.com
+    //nor I am ready to make more commits
+    //but do I need to share again???
+    //ABSOLUTELY NOT
+    //It's time to COMMIT and just PUSH!!!!!
 }
